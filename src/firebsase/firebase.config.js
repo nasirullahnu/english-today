925c0568f19a9602f3b0c5f30b502e25a62bf7c7
nasirullahnu <nasirullah878@gmail.com>
2022-10-27 03:36:47 +0600

    site created and simple route setup done

---
+++ src/firebsase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAkFsRPCQHgAsjo5S_yJ9q-wbQSqolxxM",
+  authDomain: "english-today-ca845.firebaseapp.com",
+  projectId: "english-today-ca845",
+  storageBucket: "english-today-ca845.appspot.com",
+  messagingSenderId: "154174849043",
+  appId: "1:154174849043:web:6ee1645341c83fd5043fe6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
