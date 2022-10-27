6e58223424313195a0350b35c919e768506202b3
nasirullahnu <nasirullah878@gmail.com>
2022-10-28 03:12:08 +0600

    data loaded on checkout route

--- src/shared/Checkouts/Checkouts.js
+++ src/shared/Checkouts/Checkouts.js
@@ -3,12 +3,16 @@ import { useLoaderData } from 'react-router-dom';
 
 const Checkouts = () => {
     const checkoutData = useLoaderData()
+    const {img, title, mentor, price} = checkoutData
     console.log(checkoutData);
 
     return (
         <div>
-            <h2>this is checkout page</h2>
-            <h4>{checkoutData.title}</h4>
+            <h2>Enrol Our Course</h2>
+            <img src={img} alt="" srcset="" />
+            <h4>{title}</h4>
+            <p>Mentor : {mentor.name}</p>
+            <h4>Price : {price}</h4>
         </div>
     );
 };
